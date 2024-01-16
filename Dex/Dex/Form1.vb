c8f5d81782643b45bc7ac6190202efad9d69c4f4
sentaurie <auzy.weaver@gmail.com>
2024-01-16 13:07:17 -0700

    added more code to load the data in

--- Dex/Dex/Form1.vb
+++ Dex/Dex/Form1.vb
@@ -21,15 +21,15 @@ Public Class Form1
 
     Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
         Dim outFile As New StreamWriter("data.txt")
-        outFile.WriteLine(Field1.Text)
+        outFile.Write(Field1.Text)
         outFile.Write("|")
-        outFile.WriteLine(Field2.Text)
+        outFile.Write(Field2.Text)
         outFile.Write("|")
-        outFile.WriteLine(Field3.Text)
+        outFile.Write(Field3.Text)
         outFile.Write("|")
-        outFile.WriteLine(Field4.Text)
+        outFile.Write(Field4.Text)
         outFile.Write("|")
-        outFile.WriteLine(Field5.Text)
+        outFile.Write(Field5.Text)
         outFile.Write("|")
         outFile.WriteLine(PictureBox1.ImageLocation)
         outFile.Close()
@@ -44,6 +44,21 @@ Public Class Form1
                 idx = idx + 1
             End While
             inFile.Close()
+            ShowRecord(0)
+        End If
+    End Sub
+
+    Public Sub ShowRecord(index As Integer)
+        Dim fields() As String
+        fields = records(index).Split("|")
+        Field1.Text = fields(0)
+        Field2.Text = fields(1)
+        Field3.Text = fields(2)
+        Field4.Text = fields(3)
+        Field5.Text = fields(4)
+
+        If File.Exists(fields(5)) Then
+            PictureBox1.Load(fields(5))
         End If
     End Sub
 
